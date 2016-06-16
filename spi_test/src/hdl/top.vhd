c0fdb3931db91551847ac759697b11f7d7f7da98
kjellhar <kjell'lentic.net>
2016-06-16 12:25:26 -0700

    added spi_slave from opencores.org

--- spi_test/src/hdl/top.vhd
+++ spi_test/src/hdl/top.vhd
@@ -51,26 +51,42 @@ architecture Behavioral of top is
         clk_out1          : out    std_logic);
     end component;
     
-    component spi_slave
-        Port ( clk : in STD_LOGIC;
-               enable : in STD_LOGIC;
-               spi_ss_n : in STD_LOGIC;
-               spi_clk : in STD_LOGIC;
-               spi_mosi : in STD_LOGIC;
-               spi_miso : out STD_LOGIC;
-               reg_in : out STD_LOGIC_VECTOR (7 downto 0);
-               reg_out : in STD_LOGIC_VECTOR (7 downto 0);
-               busy : out STD_LOGIC;
-               data_valid : out STD_LOGIC);
-    end component;    
+    component spi_slave 
+        Generic (   
+            N : positive := 32;                                             -- 32bit serial word length is default
+            CPOL : std_logic := '0';                                        -- SPI mode selection (mode 0 default)
+            CPHA : std_logic := '0';                                        -- CPOL = clock polarity, CPHA = clock phase.
+            PREFETCH : positive := 3);                                      -- prefetch lookahead cycles
+        Port (  
+            clk_i : in std_logic := 'X';                                    -- internal interface clock (clocks di/do registers)
+            spi_ssel_i : in std_logic := 'X';                               -- spi bus slave select line
+            spi_sck_i : in std_logic := 'X';                                -- spi bus sck clock (clocks the shift register core)
+            spi_mosi_i : in std_logic := 'X';                               -- spi bus mosi input
+            spi_miso_o : out std_logic := 'X';                              -- spi bus spi_miso_o output
+            di_req_o : out std_logic;                                       -- preload lookahead data request line
+            di_i : in  std_logic_vector (N-1 downto 0) := (others => 'X');  -- parallel load data in (clocked in on rising edge of clk_i)
+            wren_i : in std_logic := 'X';                                   -- user data write enable
+            wr_ack_o : out std_logic;                                       -- write acknowledge
+            do_valid_o : out std_logic;                                     -- do_o data valid strobe, valid during one clk_i rising edge.
+            do_o : out  std_logic_vector (N-1 downto 0);                    -- parallel output (clocked out on falling clk_i)
+            --- debug ports: can be removed for the application circuit ---
+            do_transfer_o : out std_logic;                                  -- debug: internal transfer driver
+            wren_o : out std_logic;                                         -- debug: internal state of the wren_i pulse stretcher
+            rx_bit_next_o : out std_logic;                                  -- debug: internal rx bit
+            state_dbg_o : out std_logic_vector (3 downto 0);                -- debug: internal state register
+            sh_reg_dbg_o : out std_logic_vector (N-1 downto 0)              -- debug: internal shift register
+        );                      
+     end component;
     
 
     signal clk100m : std_logic; 
     
-    signal spi_busy : std_logic;
-    signal spi_data_valid : std_logic;
-    signal spi_reg_in : std_logic_vector(7 downto 0);
-    signal spi_reg_out : std_logic_vector(7 downto 0) := "00000000";
+    signal di_req : std_logic;
+    signal do_valid : std_logic;
+    signal do : std_logic_vector(7 downto 0);
+    signal di : std_logic_vector(7 downto 0) := "00000000";
+    signal wren : std_logic := '0';
+    signal wr_ack : std_logic;
        
 begin
     clkgen : clk_wiz_0
@@ -83,22 +99,25 @@ begin
         
     clk1_en <= '1';
     
-    
     u_spi_slave : spi_slave
+        generic map (
+            N => 8
+        )
         port map (
-            clk => clk100m,
-            enable => '1',
-            spi_ss_n => PI_GPIO8,
-            spi_clk => PI_GPIO9,
-            spi_mosi => PI_GPIO10,
-            spi_miso => PI_GPIO11,
-            reg_in => spi_reg_in,
-            reg_out => spi_reg_out,
-            busy => spi_busy,
-            data_valid => spi_data_valid
+            clk_i => clk100m,
+            spi_ssel_i => PI_GPIO8,
+            spi_sck_i => PI_GPIO9,
+            spi_mosi_i => PI_GPIO10,
+            spi_miso_o => PI_GPIO11,
+            di_req_o => di_req,
+            di_i => di,
+            wren_i => wren,
+            wr_ack_o => wr_ack,
+            do_valid_o => do_valid,
+            do_o => do             
         );
-        
-        usr_led1 <= spi_reg_in(0);
-        usr_led2 <= spi_reg_in(1);
+    
+    usr_led1 <= do(0);
+    usr_led2 <= do(1);
 
 end Behavioral;
