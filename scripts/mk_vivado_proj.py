cc6e1c583a38b06b95cb6e9f2a5302f5654a3b72
kjellhar <kjell'lentic.net>
2016-06-16 03:38:20 -0700

    -

--- scripts/mk_vivado_proj.py
+++ scripts/mk_vivado_proj.py
@@ -2,6 +2,8 @@
 
 import sys
 import os
+from shutil import copyfile
+
 
 if len(sys.argv)==1:
 	print "No project name."
@@ -18,9 +20,14 @@ folders = [
 	projectName+"/src/hdl",
 	projectName+"/src/xdc",
 	projectName+"/src/hls",
+	projectName+"/src/ip",
 	projectName+"/work"]
 
 if not os.path.exists(projectName):
 	for folder in folders: 
 		os.makedirs(folder)
-		open(folder+"/README", 'a').close	
+		open(folder+"/README", 'a').close
+
+copyfile("./scripts/template_files/build_project.tcl", projectName+"/scripts/build_project.tcl")
+copyfile("./scripts/template_files/artixpi_io.xdc", projectName+"/src/xdc/artixpi_io.xdc")
+copyfile("./scripts/template_files/artixpi_config_spi.xdc", projectName+"/src/xdc/artixpi_config_spi.xdc")
